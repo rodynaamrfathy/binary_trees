8fc9aacf199311520b7addef538ed68f82d18504
rodynaamrfathy <rodynaamr@icloud.com>
2024-04-20 13:29:31 -0700

    ‘UPDATE’

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,10 +4,12 @@
 
 
 /**
- * binary_tree_insert_right - insert a new node as the right child of another node
- * If parent already has a right-child, the new node must take its place,
- * and the old right-child must be set as the right-child of the new node.
- * 
+ * binary_tree_insert_right - insert a new node as the right
+ * child of another node If parent already has a
+ * right-child, the new node must take its place,
+ * and the old right-child must be set as the
+ * right-child of the new node.
+ *
  * @parent: address of the parrent node.
  * @value: value of the new node.
  *
@@ -17,11 +19,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if(parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	new = binary_tree_node(parent,value);
-	if(parent->right != NULL)
+	new = binary_tree_node(parent, value);
+	if (parent->right != NULL)
 	{
 		new->right = parent->right;
 		parent->right->parent = new;
