a1d766c09205ab3bae4b7b1b83296ddc0b309740
rodynaamrfathy <rodynaamr@icloud.com>
2024-05-06 15:26:36 -0700

    ‘UPDATE’

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Finds the sibling of a node.
+ *
+ * @node: A pointer to the node to find the sibling.
+ *
+ * Return: NULL if node is NULL or parent is NULL or node has no sibiling.
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	return (node->parent->left);
+}
+/**
+ * binary_tree_uncle - Finds the uncle of a node.
+ *
+ * @node: A pointer to the node to find the uncle.
+ *
+ * Return: NULL if node is NULL or node has no uncle.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));	
+}
