7a8e62823fd2a35418b4d3bf96177181399326fe
rodynaamrfathy <rodynaamr@icloud.com>
2024-05-06 15:17:02 -0700

    ‘UPDATE’

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_leaves - a function that counts the leaves in a binary tree.
+ * @tree: a pointer to the root node of the tree
+ * to count the number of leaves.
+ *
+ * Return: If tree is NULL, the function must return 0.
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+
+	return (leaves);
+}
+/**
  * power - Calculates the power of a base to an exponent.
  *
  * @base: The base value.
