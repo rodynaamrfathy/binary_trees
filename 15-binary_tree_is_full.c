9b14a7db55533799c05ec55b947c2d1099b2cc35
rodynaamrfathy <rodynaamr@icloud.com>
2024-05-06 14:39:09 -0700

    ‘UPDATE’

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,13 +10,13 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-        	return (0);
+		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-        	return (1);
+		return (1);
 
 	if (tree->left == NULL || tree->right == NULL)
-        	return (0);
+		return (0);
 
 	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
